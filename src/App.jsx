8a5e8311a8b616b7bca141f930f23c617c94c713
tonny-dev <mumoantony000@gmail.com>
2025-06-13 23:16:05 +0300

    Add: readme

--- src/App.jsx
+++ src/App.jsx
@@ -173,7 +173,7 @@ function App() {
   // Render current step component
   const renderCurrentStep = () => {
     switch (currentStep) {
-      case 6:
+      case 1:
         return <PostcodeEntry onAddressSelected={handleAddressSelected} />;
 
       case 2:
@@ -216,7 +216,7 @@ function App() {
           />
         );
 
-      case 1:
+      case 6:
         return (
           <Payment
             onPaymentComplete={handlePaymentComplete}
